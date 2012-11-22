0021bc2d82060a35206ed398b7e7c333813a602e
siun <siun@localhost.localdomain>
2012-11-23 03:02:37 +0900

    色付け、reflect調整

--- script.js
+++ script.js
@@ -9,15 +9,19 @@ ObjectModel.prototype.init = function(target) {
 	this.context = this.canvas.getContext("2d");
 }
 
-var K = new function K() {
-	this.fps = 48; // 60fps==16 30fps==32 20fps==48
+var K = new function() {
+	this.fps = 48; // 60fps:16 30fps:32 20fps:48 10fps:96
 	this.name = "sBD";
 	this.width = 12;
 	this.height = 24;
+
+	// return x < num
+	this.rand = function(num) { return Math.random() * num | 0; }
 }
 
-var BALL = new function BALL() {
-	this.color = ["#e87812", "#ffd000", "#ff0000", "#dc00e0"];
+var BALL = new function() {
+	this.color = ["#e87812", "#ffd000", "#ff0000",
+				  "#dc00e0", "#00ff00", "#0000eb", "#02e4e6"];
 	this.r = 20;
 }
 
@@ -25,6 +29,7 @@ var manager = new ObjectModel();
 var board = new ObjectModel();
 var shooter = new ObjectModel();
 var curBall = new ObjectModel();
+var nextBall = new ObjectModel();
 var ballList = new ObjectModel();
 
 manager.run = function() {
@@ -39,7 +44,7 @@ manager.run = function() {
 	function init() {
 		board.init(K.name);
 		shooter.init(K.name);
-		curBall.init(K.name);
+		nextBall.init(K.name);
 		ballList.init(K.name);
 	}
 
@@ -62,6 +67,7 @@ manager.update = function() {
 	board.update();
 	shooter.update();
 	curBall.update();
+	nextBall.update();
 	ballList.update();
 }
 
@@ -96,7 +102,9 @@ shooter.run = function() {
 	});
 
 	$("#sBD").on("click", function(e) {
+		if (curBall.Firing) return;
 		curBall.init(K.name);
+		nextBall.rotate();
 		curBall.run(shooter.posM);
 	});
 }
@@ -122,7 +130,8 @@ shooter.draw = function() {
 
 curBall.init = function(target) {
 	ObjectModel.prototype.init(target);
-	this.pos = { x: board.size.x/2, y: 600, spd: 12 };
+	this.pos = { x: board.size.x/2, y: 600, spd: 26 }; //spd:26
+	this.color = nextBall.list[0].color;
 }
 
 curBall.run = function(pos) {
@@ -134,30 +143,60 @@ curBall.run = function(pos) {
 }
 
 curBall.update = function() {
+	if (!this.Firing) return;
 	this.move();
 	this.reflect();
 	this.draw();
 }
 
 curBall.move = function() {
-	if (!this.Firing) return;
-
 	this.pos.x += this.pos.dx;
 	this.pos.y += this.pos.dy;
 }
 
 curBall.reflect = function() {
-	if (this.pos.x <= 0 || this.pos.x >= board.size.x)
+	if (this.pos.x <= BALL.r || this.pos.x >= board.size.x-BALL.r)
 		this.pos.dx = -this.pos.dx;
-	if (this.pos.y >= board.size.y)
+	if (this.pos.y >= board.size.y-BALL.r)
 		this.pos.dy = -this.pos.dy;
 }
 
 curBall.draw = function() {
-//	$("#ball").text(curBall.pos.x + " " + curBall.pos.y);
 	this.context.beginPath();
+	this.context.fillStyle = this.color;
 	this.context.arc(this.pos.x, this.pos.y, BALL.r, 0, Math.PI*2, true);
 	this.context.fill();
+}
+
+//==================================================
+
+nextBall.init = function() {
+	this.list = new Array;
+	for (var i=0; i<3; ++i) {
+		this.list.push({ color: BALL.color[K.rand(BALL.color.length)],
+						 pos: { x: board.size.x/2 , y: 600 + i*BALL.r*2} });
+	}
+}
+
+nextBall.update = function() {
+	this.draw();
+}
+
+nextBall.rotate = function() {
+	this.list.splice(0, 1);
+	for (var i=0; i<this.list.length; ++i)
+		this.list[i].pos.y -= BALL.r*2;
+	this.list.push({ color: BALL.color[K.rand(BALL.color.length)],
+					 pos: { x: board.size.x/2 , y: 600 + 2*BALL.r*2} });
+}
+
+nextBall.draw = function() {
+	for (var i=0; i<this.list.length; ++i) {
+		this.context.beginPath();
+		this.context.fillStyle = this.list[i].color;
+		this.context.arc(this.list[i].pos.x, this.list[i].pos.y, BALL.r, 0, Math.PI*2, true);
+		this.context.fill();
+	}
 }
 
 //==================================================
@@ -171,7 +210,8 @@ ballList.init = function(target) {
 		this.list.push(new Array);
 		for (var o=0; o<K.width; ++o) {
 			if (i<6)
-				data = { color: "#dc00e0", pos: { x: o, y: i } };
+				data = { color: BALL.color[K.rand(BALL.color.length)],
+						 pos: { x: o, y: i } };
 			else
 				data = false;
 
@@ -194,7 +234,7 @@ ballList.move = function() {
 }
 
 ballList.add = function() {
-
+//	if (curBall.pos
 }
 
 ballList.draw = function() {
@@ -202,6 +242,7 @@ ballList.draw = function() {
 		for (var o=0; o<K.width; ++o) {
 			if (this.list[i][o] != false) {
 				this.context.beginPath();
+				this.context.fillStyle = this.list[i][o].color;
 				if (i%2==0)
 					this.context.arc(this.list[i][o].pos.x*BALL.r*2 + BALL.r,
 									 this.list[i][o].pos.y*BALL.r*2 + BALL.r,
@@ -221,10 +262,7 @@ $(function() {
 	manager.run();
 });
 
-//積もってくリスト作成(デフォルトで既にくっついてる)
 //curBallを積もってくリストに追加
-//積もってくリスト表示
-//色指定
 //そろったら積もってくリスト消すシステム
 //全部消えたらクリア
 
