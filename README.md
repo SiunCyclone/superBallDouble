354f4af29223565b475c80d5a959279b72e0744b
siun <siun@localhost.localdomain>
2012-11-22 19:50:38 +0900

    FPS制御

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 superBallDouble
 ===============
+
+制作中
--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var board = new ObjectModel();
 var shooter = new ObjectModel();
 var curBall = new ObjectModel();
 var ballList = new ObjectModel();
-var Playing = false;
+var FPS = 16; // 1000/60 #=> 16.66...8
 
 manager.run = function() {
 	$("#start").on("click", function() { 
@@ -31,59 +31,130 @@ manager.run = function() {
 	}
 
 	function run() {
-		Playing = true;
-		board.run();
+		manager.Playing = true;
 		shooter.run();
+		manager.main();
 	};
 
-	function stop() { Playing = false; };
+	function stop() { manager.Playing = false; };
+}
+
+manager.main = function() {
+	if (manager.Playing)
+		setTimeout(function() { manager.main(); }, FPS);
+	this.update();
+}
+
+manager.update = function() {
+	board.update();
+	shooter.update();
 }
 
 //==================================================
 
 //OverRide
-board.init = function() {
-	ObjectModel.prototype.init("sBD");
-	this.fps = 1000;
-}
-
-board.run = function() {
-	setTimeout(function() { board.main() } , board.fps);
-}
-
-board.main = function() {
-	if (Playing)
-		setTimeout(function() { board.main() } , board.fps);
-	this.update();
+board.init = function(target) {
+	ObjectModel.prototype.init(target);
+	this.size = { x: 500, y: 800 };
 }
 
 board.update = function() {
-	console.log("boardUpdate");
+	this.clear();
+}
+
+board.clear = function() {
+	this.context.clearRect(0, 0, this.size.x, this.size.y);
 }
 
 //==================================================
 
-shooter.pos = new Object;
+
+//OverRide
+shooter.init = function(target) {
+	ObjectModel.prototype.init(target);
+	this.pos = { x: board.size.x/2, y: 600 };
+	this.posM = { x: board.size.x/2, y: 0 };
+}
 
 shooter.run = function() {
 	$("#sBD").mousemove( function(e) {
-		shooter.pos.x = e.pageX - $("#sBD").offset()["left"];
-		shooter.pos.y = e.pageY - $("#sBD").offset()["top"];
+		shooter.move(e);
+	});
 
-		shooter.main();
+	$("#sBD").on("click", function(e) {
+		curBall.run();
 	});
 }
 
-shooter.main = function() {
+shooter.update = function() {
 	this.draw();
 }
 
-shooter.draw = function() {
-	$("#coords").text(shooter.pos.x + " " + shooter.pos.y);
+shooter.move = function(e) {
+	this.posM.x = e.pageX - $("#sBD").offset()["left"];
+	this.posM.y = e.pageY - $("#sBD").offset()["top"];
 }
+
+shooter.draw = function() {
+	$("#coords").text(shooter.posM.x + " " + shooter.posM.y);
+	this.context.beginPath();
+	this.context.moveTo(this.pos.x, this.pos.y);
+	this.context.lineTo(this.posM.x, this.posM.y);
+	this.context.stroke();
+}
+
+//==================================================
+
+curBall.Firing = false;
+curBall.pos = new Object;
+
+curBall.run = function() {
+	this.Firing = true;
+}
+
+curBall.main = function() {
+	if (manager.Playing)
+		setTimeout(function() { curBall.main() } , FPS);
+	this.update();
+}
+
+curBall.update = function() {
+	this.clear();
+	if ( this.canMove() )
+		this.move();
+	this.draw();
+}
+
+curBall.clear = function() {
+
+}
+
+curBall.canMove = function() {
+	if (!this.Firing) return;
+}
+
+curBall.move = function() {
+
+}
+
+curBall.draw = function() {
+
+}
+
+//==================================================
 
 
 $(function() {
 	manager.run();
 });
+
+//shooter表示
+//ボール表示
+//ボールクリックで発射
+//壁で反発
+//積もってくリスト作成(デフォルトで既にくっついてる)
+//curBallを積もってくリストに追加
+//積もってくリスト表示
+//色指定
+//積もってくリスト消すシステム
 
